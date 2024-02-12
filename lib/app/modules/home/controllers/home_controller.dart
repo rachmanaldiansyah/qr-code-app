9bc826dd9b88b209fac230df6ff801be06c4c4e9
RACHMAN21 <rachmanaldiansyah03@gmail.com>
2024-02-12 07:54:58 +0700

    feat: export pdf catalog products

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,21 +1,175 @@
+import 'dart:io';
+import 'dart:typed_data';
+
+import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
+import 'package:open_file/open_file.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:pdf/pdf.dart';
+import 'package:pdf/widgets.dart' as pw;
 
 class HomeController extends GetxController {
-  final count = 0.obs;
-  @override
-  void onInit() {
-    super.onInit();
-  }
+  void downloadCatalog() async {
+    final pdf = pw.Document();
 
-  @override
-  void onReady() {
-    super.onReady();
-  }
+    pdf.addPage(
+      pw.Page(
+        pageFormat: PdfPageFormat.a4,
+        build: (context) {
+          return pw.Column(
+            children: [
+              pw.Center(
+                child: pw.Text(
+                  "KATALOG PRODUK",
+                  textAlign: pw.TextAlign.center,
+                  style: pw.TextStyle(
+                    fontSize: 24,
+                  ),
+                ),
+              ),
+              pw.SizedBox(height: 20),
+              pw.Table(
+                border: pw.TableBorder.all(
+                  color: PdfColor.fromHex("#000000"),
+                  width: 2,
+                ),
+                children: [
+                  pw.TableRow(
+                    children: [
+                      // No
+                      pw.Padding(
+                        padding: pw.EdgeInsets.all(15),
+                        child: pw.Text(
+                          "No",
+                          textAlign: pw.TextAlign.center,
+                          style: pw.TextStyle(
+                            fontSize: 12,
+                          ),
+                        ),
+                      ),
+                      // Kode Barang
+                      pw.Padding(
+                        padding: pw.EdgeInsets.all(15),
+                        child: pw.Text(
+                          "Kode Barang",
+                          textAlign: pw.TextAlign.center,
+                          style: pw.TextStyle(
+                            fontSize: 12,
+                          ),
+                        ),
+                      ),
+                      // Nama Barang
+                      pw.Padding(
+                        padding: pw.EdgeInsets.all(15),
+                        child: pw.Text(
+                          "Nama Barang",
+                          textAlign: pw.TextAlign.center,
+                          style: pw.TextStyle(
+                            fontSize: 12,
+                          ),
+                        ),
+                      ),
+                      // Qty
+                      pw.Padding(
+                        padding: pw.EdgeInsets.all(15),
+                        child: pw.Text(
+                          "Qty",
+                          textAlign: pw.TextAlign.center,
+                          style: pw.TextStyle(
+                            fontSize: 12,
+                          ),
+                        ),
+                      ),
+                      // QR Code
+                      pw.Padding(
+                        padding: pw.EdgeInsets.all(15),
+                        child: pw.Text(
+                          "Kode Qr",
+                          textAlign: pw.TextAlign.center,
+                          style: pw.TextStyle(
+                            fontSize: 12,
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
 
-  @override
-  void onClose() {
-    super.onClose();
-  }
+                  // ISI DATA PRODUK
+                  ...List.generate(5, (index) {
+                    return pw.TableRow(
+                      children: [
+                        // No
+                        pw.Padding(
+                          padding: pw.EdgeInsets.all(15),
+                          child: pw.Text(
+                            "${index + 1}",
+                            style: pw.TextStyle(
+                              fontSize: 12,
+                            ),
+                          ),
+                        ),
+                        // Kode Barang
+                        pw.Padding(
+                          padding: pw.EdgeInsets.all(15),
+                          child: pw.Text(
+                            "Kode Barang",
+                            style: pw.TextStyle(
+                              fontSize: 12,
+                            ),
+                          ),
+                        ),
+                        // Nama Barang
+                        pw.Padding(
+                          padding: pw.EdgeInsets.all(15),
+                          child: pw.Text(
+                            "Nama Barang",
+                            style: pw.TextStyle(
+                              fontSize: 12,
+                            ),
+                          ),
+                        ),
+                        // Qty
+                        pw.Padding(
+                          padding: pw.EdgeInsets.all(15),
+                          child: pw.Text(
+                            "Qty",
+                            style: pw.TextStyle(
+                              fontSize: 12,
+                            ),
+                          ),
+                        ),
+                        // QR Code
+                        pw.Padding(
+                          padding: pw.EdgeInsets.all(15),
+                          child: pw.Text(
+                            "Kode Qr",
+                            style: pw.TextStyle(
+                              fontSize: 12,
+                            ),
+                          ),
+                        )
+                      ],
+                    );
+                  })
+                ],
+              ),
+            ],
+          );
+        },
+      ),
+    );
 
-  void increment() => count.value++;
+    // simpan pdf
+    Uint8List bytes = await pdf.save();
+
+    // buat file kosong di direktori
+    final dir = await getApplicationDocumentsDirectory();
+    final file = File('${dir.path}/katalog.pdf');
+
+    // memasukkan data bytes -> file kosong
+    await file.writeAsBytes(bytes);
+
+    // open pdf
+    await OpenFile.open(file.path);
+  }
 }
